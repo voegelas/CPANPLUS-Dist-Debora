9f05928954271dad5bebccc02bacec41d2759c15
Andreas Vögele <voegelas@cpan.org>
2022-01-14 09:05:18 +0100

    Set the Debian Standards-Version to 4.6.0

--- lib/CPANPLUS/Dist/Debora/Package/Debian.pm
+++ lib/CPANPLUS/Dist/Debora/Package/Debian.pm
@@ -369,7 +369,7 @@ Maintainer: [% $package->packager %]
 Section: perl
 Priority: optional
 Build-Depends: debhelper-compat (= 12)
-Standards-Version: 4.5.1
+Standards-Version: 4.6.0
 Homepage: [% $package->url %]
 
 Package: [% $package->name %]
