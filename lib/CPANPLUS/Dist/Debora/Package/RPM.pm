9e6373784c261dcfb97a32e98bbfb1882a310c79
Andreas Vögele <voegelas@cpan.org>
2024-10-30 19:05:27 +0100

    Bump version

--- lib/CPANPLUS/Dist/Debora/Package/RPM.pm
+++ lib/CPANPLUS/Dist/Debora/Package/RPM.pm
@@ -6,7 +6,7 @@ use 5.016;
 use warnings;
 use utf8;
 
-our $VERSION = '0.013';
+our $VERSION = '0.014';
 
 use parent qw(CPANPLUS::Dist::Debora::Package);
 
@@ -571,7 +571,7 @@ CPANPLUS::Dist::Debora::Package::RPM - Create binary RPM packages
 
 =head1 VERSION
 
-version 0.013
+version 0.014
 
 =head1 SYNOPSIS
 
